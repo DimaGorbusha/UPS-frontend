1745435b20c93e10f9eb733d6f3d50f6cb86e471
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-10-22 15:21:55 +0500

    Add: общая разметка кнопки регистрации

---
+++ src/components/SignUpButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignUpButton(props) {
+  return (
+    <div>
+        <p className='font-squartiqa text-[24px]' onClick={props.func}>РЕГИСТРАЦИЯ</p>
+    </div>
+  )
+}
