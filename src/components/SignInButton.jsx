b3b4eb77554bedd872c8afb8355708ebfc81dbb8
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-10-22 15:01:25 +0500

    Add: installed tailwind and font

---
+++ src/components/SignInButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignInButton(props) {
+  return (
+    <div>
+        <p className='font-squartiqa text-[24px]' onClick={props.func}>ВХОД</p>
+    </div>
+  )
+}
